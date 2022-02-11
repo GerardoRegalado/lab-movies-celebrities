3ac6ffc95ec3ebc5501220bdb0513f1280cd226b
GerardoRegalado <gerardo.regalado24@gmail.com>
2022-02-11 10:43:05 -0600

    index view already working

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@
 const express			= require("express")
 const router			= express.Router()
 
-const indexController 	= require("./../controllers/indexController")
+const indexController 	= require("../controllers/indexController")
 
 
 // 2. ROUTER
---
+++ routes/movies.routes.js
@@ -0,0 +1,18 @@
+// 1. IMPORTACIONES
+const express			= require("express")
+const router			= express.Router()
+
+
+
+
+//2. RUTEO
+
+
+
+
+
+
+
+
+//3. EXPORTACION
+module.exports= router
